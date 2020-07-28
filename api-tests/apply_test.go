39714fbe6f3cadfd5cd9a0a648b5723e54b58803
paval <pavel.kasko@percona.com>
2020-07-28 10:11:31 +0300

    Add pxc/psmdb tests for returned versions

--- api-tests/apply_test.go
+++ api-tests/apply_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Percona-Lab/percona-version-service/client"
 	"github.com/Percona-Lab/percona-version-service/client/models"
 	"github.com/Percona-Lab/percona-version-service/client/version_service"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_apply_should_return_just_one_version(t *testing.T) {
@@ -22,11 +23,10 @@ func Test_apply_should_return_just_one_version(t *testing.T) {
 	pxcParams.WithTimeout(2 * time.Second)
 
 	pxcResp, err := cli.VersionService.VersionServiceApply(pxcParams)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
-	if len(pxcResp.Payload.Versions[0].Matrix.Pxc) != 1 ||
+	if len(pxcResp.Payload.Versions) != 1 ||
+		len(pxcResp.Payload.Versions[0].Matrix.Pxc) != 1 ||
 		len(pxcResp.Payload.Versions[0].Matrix.Backup) != 1 ||
 		len(pxcResp.Payload.Versions[0].Matrix.Proxysql) != 1 ||
 		len(pxcResp.Payload.Versions[0].Matrix.Pmm) != 1 ||
@@ -43,11 +43,10 @@ func Test_apply_should_return_just_one_version(t *testing.T) {
 	psmdbParams.WithTimeout(2 * time.Second)
 
 	psmdbResp, err := cli.VersionService.VersionServiceApply(psmdbParams)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 
-	if len(psmdbResp.Payload.Versions[0].Matrix.Mongod) != 1 ||
+	if len(pxcResp.Payload.Versions) != 1 ||
+		len(psmdbResp.Payload.Versions[0].Matrix.Mongod) != 1 ||
 		len(psmdbResp.Payload.Versions[0].Matrix.Backup) != 1 ||
 		len(psmdbResp.Payload.Versions[0].Matrix.Pmm) != 1 ||
 		len(psmdbResp.Payload.Versions[0].Matrix.Operator) != 1 {
@@ -68,9 +67,7 @@ func Test_apply_pxc_should_return_same_major_version(t *testing.T) {
 	for _, v := range []string{"5.7", "8.0"} {
 		pxcParams.DatabaseVersion = &v
 		pxcResp, err := cli.VersionService.VersionServiceApply(pxcParams)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 
 		k := getVersion(pxcResp.Payload.Versions[0].Matrix.Pxc)
 		if !strings.HasPrefix(k, v) {
@@ -92,14 +89,108 @@ func Test_apply_psmdb_should_return_same_major_version(t *testing.T) {
 	for _, v := range []string{"4.2", "4.0", "3.6"} {
 		psmdbParams.DatabaseVersion = &v
 		psmdbResp, err := cli.VersionService.VersionServiceApply(psmdbParams)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 
 		k := getVersion(psmdbResp.Payload.Versions[0].Matrix.Mongod)
 		if !strings.HasPrefix(k, v) {
 			t.Errorf("wrong version returned: %s", k)
 		}
+	}
+}
+
+func Test_apply_pxc_returned_versions(t *testing.T) {
+	cli := cli()
+
+	v57 := "5.7"
+
+	cases := []struct {
+		apply     string
+		operator  string
+		dbVersion *string
+		version   string
+	}{
+		// test latest
+		{"latest", "1.5.0", nil, "8.0.19-10.1"},
+		{"latest", "1.4.0", nil, "8.0.18-9.3"},
+		{"latest", "1.5.0", &v57, "5.7.30-31.43"},
+		{"latest", "1.4.0", &v57, "5.7.28-31.41.2"},
+
+		// test recommended
+		{"recommended", "1.5.0", nil, "8.0.19-10.1"},
+		{"recommended", "1.4.0", nil, "8.0.18-9.3"},
+		{"recommended", "1.5.0", &v57, "5.7.30-31.43"},
+		{"recommended", "1.4.0", &v57, "5.7.28-31.41.2"},
+
+		// test exact
+		{"5.7.28-31.41.2", "1.5.0", nil, "5.7.28-31.41.2"},
+		{"5.7.28-31.41.2", "1.4.0", nil, "5.7.28-31.41.2"},
+		{"8.0.19-10.1", "1.5.0", nil, "8.0.19-10.1"},
+		{"8.0.18-9.3", "1.4.0", nil, "8.0.18-9.3"},
+	}
+
+	for _, c := range cases {
+		params := &version_service.VersionServiceApplyParams{
+			Apply:           c.apply,
+			OperatorVersion: c.operator,
+			Product:         "pxc-operator",
+		}
+		params.WithTimeout(2 * time.Second)
+		if c.dbVersion != nil {
+			params.DatabaseVersion = c.dbVersion
+		}
+
+		resp, err := cli.VersionService.VersionServiceApply(params)
+		assert.NoError(t, err)
+
+		v := getVersion(resp.Payload.Versions[0].Matrix.Pxc)
+		assert.Equal(t, c.version, v)
+	}
+}
+
+func Test_apply_psmdb_returned_versions(t *testing.T) {
+	cli := cli()
+
+	v36 := "3.6"
+	v40 := "4.0"
+
+	cases := []struct {
+		apply     string
+		operator  string
+		dbVersion *string
+		version   string
+	}{
+		// test latest
+		{"latest", "1.5.0", nil, "4.2.8-8"},
+		{"latest", "1.5.0", &v40, "4.0.19-12"},
+		{"latest", "1.5.0", &v36, "3.6.18-6.0"},
+
+		// test recommended
+		{"recommended", "1.5.0", nil, "4.2.8-8"},
+		{"recommended", "1.5.0", &v40, "4.0.19-12"},
+		{"recommended", "1.5.0", &v36, "3.6.18-6.0"},
+
+		// test exact
+		{"4.2.7-7", "1.5.0", nil, "4.2.7-7"},
+		{"4.0.18-11", "1.5.0", nil, "4.0.18-11"},
+		{"3.6.18-5.0", "1.5.0", nil, "3.6.18-5.0"},
+	}
+
+	for _, c := range cases {
+		params := &version_service.VersionServiceApplyParams{
+			Apply:           c.apply,
+			OperatorVersion: c.operator,
+			Product:         "psmdb-operator",
+		}
+		params.WithTimeout(2 * time.Second)
+		if c.dbVersion != nil {
+			params.DatabaseVersion = c.dbVersion
+		}
+
+		resp, err := cli.VersionService.VersionServiceApply(params)
+		assert.NoError(t, err)
+
+		v := getVersion(resp.Payload.Versions[0].Matrix.Mongod)
+		assert.Equal(t, c.version, v)
 	}
 }
 
