4f4da2b8f94c8e9e98718dbf3a8dcd1eaf09c216
Tomislav Plavcic <tomislav.plavcic@percona.com>
2023-09-05 09:29:42 +0200

    K8SPS-267 - Add PS operator 0.6.0 (#172)

--- api-tests/operator_route_test.go
+++ api-tests/operator_route_test.go
@@ -44,6 +44,7 @@ func TestOperatorRouteShouldReturnRightOperatorVersion(t *testing.T) {
 		{"pg-operator", "2.1.0"},
 		{"pg-operator", "2.2.0"},
 		{"ps-operator", "0.5.0"},
+		{"ps-operator", "0.6.0"},
 	}
 
 	for _, c := range cases {
@@ -211,6 +212,7 @@ func TestOperatorRoutePsShouldReturnNotEmptyResponses(t *testing.T) {
 		version string
 	}{
 		{"ps-operator", "0.5.0"},
+		{"ps-operator", "0.6.0"},
 	}
 
 	for _, c := range cases {
