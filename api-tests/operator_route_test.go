765d43939f9516375a0a2ccf36c7fc4bcf28c65a
Pavel Tankov <4014969+ptankov@users.noreply.github.com>
2024-08-20 19:00:15 +0300

    Remove PSMDB operator 1.17.0 from test case

--- api-tests/operator_route_test.go
+++ api-tests/operator_route_test.go
@@ -42,7 +42,6 @@ func TestOperatorRouteShouldReturnRightOperatorVersion(t *testing.T) {
 		{"psmdb-operator", "1.16.0"},
 		{"psmdb-operator", "1.16.1"},
 		{"psmdb-operator", "1.16.2"},
-		{"psmdb-operator", "1.17.0"},
 		{"pg-operator", "1.1.0"},
 		{"pg-operator", "1.2.0"},
 		{"pg-operator", "1.3.0"},
