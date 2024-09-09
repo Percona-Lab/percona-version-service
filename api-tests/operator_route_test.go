8c43f29695b8a4ebcc58c93f232b77461f7e187d
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2024-09-09 12:48:19 +0300

    K8SPSMDB-1098: Add PSMDB operator 1.17.0

--- api-tests/operator_route_test.go
+++ api-tests/operator_route_test.go
@@ -42,6 +42,7 @@ func TestOperatorRouteShouldReturnRightOperatorVersion(t *testing.T) {
 		{"psmdb-operator", "1.16.0"},
 		{"psmdb-operator", "1.16.1"},
 		{"psmdb-operator", "1.16.2"},
+		{"psmdb-operator", "1.17.0"},
 		{"pg-operator", "1.1.0"},
 		{"pg-operator", "1.2.0"},
 		{"pg-operator", "1.3.0"},
@@ -144,6 +145,7 @@ func TestOperatorRoutePsmdbShouldReturnNotEmptyResponses(t *testing.T) {
 		{"psmdb-operator", "1.16.0"},
 		{"psmdb-operator", "1.16.1"},
 		{"psmdb-operator", "1.16.2"},
+		{"psmdb-operator", "1.17.0"},
 	}
 
 	for _, c := range cases {
