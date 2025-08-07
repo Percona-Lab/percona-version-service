181fb0109bccfffa8de9278317cd3e6dc52195ff
Eleonora Zinchenko <eleonora.zinchenko@percona.com>
2025-08-07 14:32:40 +0300

    Update PXC operator with 1.18.0 images (#425)
    * Update PXC operator with 1.18.0 images
    
    * Add 8.4 support
    
    * Add PMM3 support
    ---------
    
    Co-authored-by: Ege Güneş <ege.gunes@percona.com>

--- api-tests/operator_route_test.go
+++ api-tests/operator_route_test.go
@@ -33,6 +33,7 @@ func TestOperatorRouteShouldReturnRightOperatorVersion(t *testing.T) {
 		{"pxc-operator", "1.16.0"},
 		{"pxc-operator", "1.16.1"},
 		{"pxc-operator", "1.17.0"},
+		{"pxc-operator", "1.18.0"},
 		{"psmdb-operator", "1.5.0"},
 		{"psmdb-operator", "1.6.0"},
 		{"psmdb-operator", "1.7.0"},
@@ -118,6 +119,7 @@ func TestOperatorRoutePxcShouldReturnNotEmptyResponses(t *testing.T) {
 		{"pxc-operator", "1.16.0"},
 		{"pxc-operator", "1.16.1"},
 		{"pxc-operator", "1.17.0"},
+		{"pxc-operator", "1.18.0"},
 	}
 
 	for _, c := range cases {
