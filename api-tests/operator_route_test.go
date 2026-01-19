0df1c95dec79b2c8dd753fef86710e863cff855c
Julio Pasinatto <julio.pasinatto@percona.com>
2026-01-19 09:02:56 -0300

    K8SPXC-1699 Update PXC operator with 1.19.0 images

--- api-tests/operator_route_test.go
+++ api-tests/operator_route_test.go
@@ -34,6 +34,7 @@ func TestOperatorRouteShouldReturnRightOperatorVersion(t *testing.T) {
 		{"pxc-operator", "1.16.1"},
 		{"pxc-operator", "1.17.0"},
 		{"pxc-operator", "1.18.0"},
+		{"pxc-operator", "1.19.0"},
 		{"psmdb-operator", "1.5.0"},
 		{"psmdb-operator", "1.6.0"},
 		{"psmdb-operator", "1.7.0"},
@@ -128,6 +129,7 @@ func TestOperatorRoutePxcShouldReturnNotEmptyResponses(t *testing.T) {
 		{"pxc-operator", "1.16.1"},
 		{"pxc-operator", "1.17.0"},
 		{"pxc-operator", "1.18.0"},
+		{"pxc-operator", "1.19.0"},
 	}
 
 	for _, c := range cases {
